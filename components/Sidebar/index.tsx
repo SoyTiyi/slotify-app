6c26af668797a90bc299337b54431b89cf7593c8
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-18 17:50:58 -0500

    add index.ts implementation for Sidebar component

---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,5 @@
+export { default as Sidebar } from "./Sidebar";
+export { default as SidebarMobile } from "./SidebarMobile";
+export { default as SidebarNav } from "./SidebarNav";
+export { default as SidebarNavItem } from "./SidebarNavItem";
+export { default as SidebarUserSection } from "./SidebarUserSection";
