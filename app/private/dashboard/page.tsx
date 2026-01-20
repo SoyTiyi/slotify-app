79d3befd0e70117755057b96b688eb8d9e278332
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-20 08:24:30 -0600

    update dashboard page

--- app/private/dashboard/page.tsx
+++ app/private/dashboard/page.tsx
@@ -1,10 +1,4 @@
-import { apiServer } from "@/lib/api";
-import { usersApiServer } from "@/lib/features/users/users.api";
-
 const Dashboard = async () => {
-  const users = usersApiServer(apiServer);
-  const me = await users.me();
-
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold text-text-primary">Dashboard</h1>
