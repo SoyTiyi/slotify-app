be84ed1ea40e26756a3c854e63c2b69b162b8338
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-20 08:24:52 -0600

    create calendar component

---
+++ app/private/calendar/page.tsx
@@ -0,0 +1,9 @@
+const Calendar = async () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-text-primary">Dashboard</h1>
+    </div>
+  );
+};
+
+export default Calendar;
